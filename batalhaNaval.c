27cb219a38cafcdae3aded62a1e98e55b9f19989
Kauancode12 <kakabranco39@gmail.com>
2025-03-13 17:37:02 +0000

    exibição do tabuleiro adicionado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,46 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
     int tabuleiro[10][10];
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int navio1 = {3}, navio2 = {3};
+
+    for (int i = 0; i < 10; i++) {
+
+        if (i == 0)
+        {
+            printf("   A ");
+        } else {
+        
+            printf("%c ", linha[i]);
+
+        }
+        
+    }
+        
+    printf("\n");
+
+   
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
 
       
 
