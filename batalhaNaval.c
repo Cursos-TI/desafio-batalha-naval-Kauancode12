270599709afe50e25a616c46bf3f0e09b018672c
Kauancode12 <kakabranco39@gmail.com>
2025-03-14 14:11:14 +0000

    navios implementados

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
     int tabuleiro[10][10];
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int navio1 = 3, navio2 = 6;
 
+    //Exibe as letras acima do mar, para servir como coordenada.
     for (int i = 0; i < 10; i++) {
 
         if (i == 0)
@@ -21,18 +20,30 @@ int main() {
         }
         
     }
-        
+
     printf("\n");
 
-   
+    //Força o tabuleiro a ser composto completamente por zero.
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++)
         {
             tabuleiro[i][j] = 0;
         }
+    
+    }
+    
+    //Exibe os navios por meio de um loop que percorre as posições do tabuleiro e coloca 3 no lugar de 0.
+    for (int i = 0; i < 3; i++, navio1++) {
+        tabuleiro[navio1][3] = 3;
     }
 
+    for (int i = 0; i < 3; i++, navio2++) {
+        tabuleiro[1][navio2] = 3;
+    }
 
+    
+
+    //Exibe o tabuleiro e os números a esquerda que servem de coordenadas.
     for (int i = 0; i < 10; i++) {
         printf("%2d ", i + 1);
 
