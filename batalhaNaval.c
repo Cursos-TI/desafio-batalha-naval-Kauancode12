8b086dc6160911395855e124755221c7a765aa6b
Kauancode12 <kakabranco39@gmail.com>
2025-03-16 14:10:23 +0000

    habilidades representadas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,7 +5,8 @@ int main() {
     int tabuleiro[10][10];
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int navio1 = 3, navio2 = 6;
+    int navio1 = 4, navio2 = 6;
+    int pontoOrigem = 4, pontoOrigem2 = 8, pontoOrigem3 = 2;
 
     //Exibe as letras acima do mar, para servir como coordenada.
     for (int i = 0; i < 10; i++) {
@@ -47,6 +48,49 @@ int main() {
 
     for (int i = 8; i < 11; i++) {
         tabuleiro[i][3-i] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++)
+        {
+            if (j >= pontoOrigem - i && j <= pontoOrigem + i) {
+                tabuleiro[i + 1][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 0 && j == pontoOrigem2) {
+                tabuleiro[i + 3][j] = 1;
+            } 
+                
+                else if (i == 1 && j >= pontoOrigem2 - 1 && j <= pontoOrigem2 + 1) {
+                tabuleiro[i + 3][j] = 1;
+            } 
+            
+                else if (i == 2 && j == pontoOrigem2) {
+                tabuleiro[i + 3][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 1 && j >= pontoOrigem3 - 2 && j <= pontoOrigem3 + 2) {
+                tabuleiro[i + 6][j] = 1;
+            } 
+                
+                else if (i == 0 && j == pontoOrigem3) {
+                tabuleiro[i + 6][j] = 1;
+            } 
+            
+                else if (i == 2 && j == pontoOrigem3) {
+                tabuleiro[i + 6][j] = 1;
+            }
+        }
     }
 
     
